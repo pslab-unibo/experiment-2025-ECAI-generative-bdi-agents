0e5fae68a01506eb1bee86863783c24d8af853f7
Riccardo Battistini <riccardo.battistini@protonmail.com>
2025-02-10 20:57:45 +0100

    refactor: remove unused code

--- jakta-bdi/src/main/kotlin/it/unibo/jakta/agents/bdi/impl/LLMPlan.kt
+++
@@ -1,7 +0,0 @@
-package it.unibo.jakta.agents.bdi.impl
-
-import it.unibo.jakta.agents.bdi.plans.Plan
-
-interface LLMPlan {
-    val plans: Iterable<Plan>
-}
--- jakta-llm/src/main/kotlin/it/unibo/jakta/llm/output/ChatMessage.kt
+++
@@ -1,6 +0,0 @@
-package it.unibo.jakta.llm.output
-
-data class ChatMessage(
-    val thought: String,
-    val action: String,
-)
