e192e21424c2a2b5c0d80647ddfb37e3e01c2d0f
Riccardo Battistini <riccardo.battistini@protonmail.com>
2025-05-21 17:48:57 +0200

    chore: remove unused files

--- jakta-toolbox/src/lib/common.ts
+++
@@ -1,5 +0,0 @@
-export function getFilenameWithoutExtension(filePath: string): string {
-	if (!filePath) return ''
-	const filename = filePath.split(/[\\/]/).pop() || ''
-	return filename.replace(/\.[^/.]+$/, '')
-}
