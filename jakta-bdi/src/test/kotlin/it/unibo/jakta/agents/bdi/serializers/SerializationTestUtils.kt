4da57c130bff8ea21ec05cf26706b9575fecc32e
Riccardo Battistini <riccardo.battistini@protonmail.com>
2025-06-26 17:55:06 +0200

    fix(jakta-bdi): load koin modules for serialization test

--- jakta-bdi/src/test/kotlin/it/unibo/jakta/agents/bdi/serializers/SerializationTestUtils.kt
+++ jakta-bdi/src/test/kotlin/it/unibo/jakta/agents/bdi/serializers/SerializationTestUtils.kt
@@ -3,28 +3,14 @@ package it.unibo.jakta.agents.bdi.serializers
 import io.kotest.matchers.shouldBe
 import it.unibo.jakta.agents.bdi.engine.Jakta.dropNumbers
 import it.unibo.jakta.agents.bdi.engine.beliefs.Belief
+import it.unibo.jakta.agents.bdi.engine.depinjection.JaktaKoin.engineJsonModule
+import it.unibo.jakta.agents.bdi.engine.depinjection.JaktaKoin.jsonModule
 import it.unibo.jakta.agents.bdi.engine.events.Trigger
 import it.unibo.jakta.agents.bdi.engine.formatters.DefaultFormatters.termFormatter
 import it.unibo.jakta.agents.bdi.engine.goals.Goal
-import it.unibo.jakta.agents.bdi.engine.serialization.modules.JsonModule
-import it.unibo.jakta.agents.bdi.engine.serialization.modules.SerializersModuleProvider
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.SerializersModule
-import org.koin.core.context.GlobalContext
-import org.koin.dsl.module
-import org.koin.ksp.generated.module
 
 internal object SerializationTestUtils {
-    val jsonModule =
-        module {
-            single {
-                val modules = GlobalContext.get().getAll<SerializersModuleProvider>()
-                val combined = SerializersModule { modules.forEach { include(it.modules) } }
-                Json { serializersModule = combined }
-            }
-        }
-
-    internal val modules = listOf(JsonModule().module, jsonModule)
+    internal val modules = listOf(engineJsonModule, jsonModule)
 
     private fun String.normalizeJsonWhitespace(): String =
         this.trim().replace("\\s+(?=([^\"]*\"[^\"]*\")*[^\"]*$)".toRegex(), "")
